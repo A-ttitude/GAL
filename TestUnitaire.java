d4cf18fe11bf5d2fad3bc96f6e21ad435843ab17
A-ttitude <a-ttitude@A-ttitude-VAIO.(none)>
2013-09-11 13:38:32 +0200

    Correction fonctionnement test unitaire 1.

--- TestUnitaire.java
+++ TestUnitaire.java
@@ -17,18 +17,35 @@ public class TestUnitaire extends TestCase {
 	public void testPut_Ring1() {
 
 		Yinsh _yinsh = new Yinsh();
-	
-		assertTrue(_yinsh.put_ring('A', 1, Yinsh.color.BLACK));
+		
+		_yinsh.put_ring('A', 3, Yinsh.color.BLACK);
+		
+		assertTrue(_yinsh.isAnneau('A', 3));
 	}
-	
+
 	public void testPut_Ring2() {
 
 		Yinsh _yinsh = new Yinsh();
 		
 		assertTrue(_yinsh.getNbAnneau() == 0);
 		
-		_yinsh.put_ring('A', 1, Yinsh.color.BLACK);
+		_yinsh.put_ring('A', 3, Yinsh.color.BLACK);
 		
 		assertTrue(_yinsh.getNbAnneau() != 0);
+	}
+
+	public void testPut_Ring3() {
+
+		Yinsh _yinsh = new Yinsh();
+		
+		try {
+			
+			_yinsh.put_ring('A', 1, Yinsh.color.WHITE);
+			assertTrue(false);
+		}
+		catch(Exception e) {
+			
+			assertTrue(true);
+		}
 	}
 }
--- Yinsh.java
+++ Yinsh.java
@@ -25,27 +25,9 @@ public class Yinsh {
 		return Math.random() > 0.5 ? color.BLACK : color.WHITE;
 	}
 
-	public boolean put_ring(char lettreColonne, int ligne, Yinsh.color couleur) {
-
-		int colonne = -1;
-		boolean booleen = false;
-
-		switch(lettreColonne) {
-
-			case 'A': colonne = 0; break;
-			case 'B': colonne = 1; break;
-			case 'C': colonne = 2; break;
-			case 'D': colonne = 3; break;
-			case 'E': colonne = 4; break;
-			case 'F': colonne = 5; break;
-			case 'G': colonne = 6; break;
-			case 'H': colonne = 7; break;
-			case 'I': colonne = 8; break;
-			case 'J': colonne = 9; break;
-			case 'K': colonne = 10; break;
-
-			default: colonne = -1;
-		}
+	public void put_ring(char lettreColonne, int ligne, Yinsh.color couleur) {
+		
+		int colonne = Character.getNumericValue(lettreColonne) - 10;
 
 		if(colonne != -1 && ligne > 0 && ligne < 12) {
 
@@ -53,12 +35,14 @@ public class Yinsh {
 				
 				_plateau[ligne - 1][colonne] = couleur;
 
-				booleen = true;
 				_nbAnneau++;
 			}
 		}
-
-		return booleen;
+	}
+	
+	public boolean isAnneau(char lettreColonne, int ligne) {
+		
+		return _plateau[ligne - 1][Character.getNumericValue(lettreColonne) - 10] != null;
 	}
 	
 	public int getNbAnneau() {
