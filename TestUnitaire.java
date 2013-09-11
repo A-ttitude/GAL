8f5eb961fc0fcf67fbb5e2511db78ece0fdca3b9
A-ttitude <a-ttitude@A-ttitude-VAIO.(none)>
2013-09-11 15:15:06 +0200

    Test unitaire verifierCoordonnees.

--- TestUnitaire.java
+++ TestUnitaire.java
@@ -18,8 +18,9 @@ public class TestUnitaire extends TestCase {
 
 		Yinsh _yinsh = new Yinsh();
 		
-		_yinsh.put_ring('A', 3, Yinsh.color.BLACK);
-		
+		try { _yinsh.put_ring('A', 3, Yinsh.color.BLACK); }
+		catch(Exception e) {}
+
 		assertTrue(_yinsh.isAnneau('A', 3));
 	}
 
@@ -29,12 +30,25 @@ public class TestUnitaire extends TestCase {
 		
 		assertTrue(_yinsh.getNbAnneau() == 0);
 		
-		_yinsh.put_ring('A', 3, Yinsh.color.BLACK);
+		try { _yinsh.put_ring('A', 3, Yinsh.color.BLACK); }
+		catch(Exception e) {}
 		
 		assertTrue(_yinsh.getNbAnneau() != 0);
 	}
 
 	public void testPut_Ring3() {
+		
+		// A > 1, 6, 7, 8, 9, 10, 11
+		// B > 8, 9, 10, 11
+		// C > 9, 10, 11
+		// D > 10, 11
+		// E > 11
+		// F > 1, 11
+		// G > 1
+		// H > 1, 2
+		// I > 1, 2, 3
+		// J > 1, 2, 3, 4
+		// K > 1, 2, 3, 4, 5, 6, 11
 
 		Yinsh _yinsh = new Yinsh();
 		
--- Yinsh.java
+++ Yinsh.java
@@ -1,17 +1,17 @@
 public class Yinsh {
 
 	// Variables //
-	
+
 	public enum color {
 
 		BLACK, WHITE
 	}
 
 	public color[][]	_plateau;
-	public int 			_nbAnneau;
+	public int			_nbAnneau;
 
 	// Constructeur
-	
+
 	public Yinsh() {
 
 		_plateau = new color[11][11];
@@ -19,34 +19,75 @@ public class Yinsh {
 	}
 
 	// Méthodes
-	
+
 	public color current_color() {
 
 		return Math.random() > 0.5 ? color.BLACK : color.WHITE;
 	}
 
-	public void put_ring(char lettreColonne, int ligne, Yinsh.color couleur) {
-		
+	public void put_ring(char lettreColonne, int ligne, Yinsh.color couleur) throws Exception {
+
 		int colonne = Character.getNumericValue(lettreColonne) - 10;
 
-		if(colonne != -1 && ligne > 0 && ligne < 12) {
+		if(verifierCoordonnees(colonne, ligne - 1)) {
 
 			if(_plateau[ligne - 1][colonne] == null) {
-				
+
 				_plateau[ligne - 1][colonne] = couleur;
 
 				_nbAnneau++;
 			}
 		}
 	}
-	
+
 	public boolean isAnneau(char lettreColonne, int ligne) {
-		
+
 		return _plateau[ligne - 1][Character.getNumericValue(lettreColonne) - 10] != null;
 	}
-	
+
 	public int getNbAnneau() {
-		
+
 		return _nbAnneau;
+	}
+
+	private boolean verifierCoordonnees(int colonne, int ligne) throws Exception {
+
+		if(colonne < 0 || colonne > 11 || ligne < 0 || ligne > 11)
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 0 && (ligne == 0 || ligne >= 5)) // Si A | 1, 6, 7, 8, 9, 10, 11
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 1 && ligne >= 7) // Si B | 8, 9, 10, 11
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 2 && ligne >= 8) // Si C | 9, 10, 11
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 3 && ligne >= 9) // Si D | 10, 11
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 4 && ligne == 10) // Si E | 10
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 5 && (ligne == 0 || ligne == 10)) // Si F | 10
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 6 && ligne == 0) // Si G | 1
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 7 && ligne < 2) // Si H | 1, 2
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 8 && ligne < 3) // Si I | 1, 2, 3
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 9 && ligne < 4) // Si J | 1, 2, 3, 4
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		if(colonne == 10 && (ligne < 6 || ligne == 10)) // Si K | 1, 2, 3, 4, 5, 6, 11
+			throw new Exception("/!\\ Coordonnees non valide.");
+
+		return true;
 	}
 }
